6bf3472ab462ba54415ceb8b7058d674f5a66a85
michelefatticcioni <michelefatticcioni@gmail.com>
2025-08-26 18:10:55 +0200

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,19 @@
+export const SPN_SYSTEM = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+SPN_SYSTEM.abilities = {
+  str: "SPN_SYSTEM.Ability.Str.long",
+  dex: "SPN_SYSTEM.Ability.Dex.long",
+  int: "SPN_SYSTEM.Ability.Int.long",
+  wis: "SPN_SYSTEM.Ability.Wis.long",
+};
+
+SPN_SYSTEM.abilityAbbreviations = {
+  str: "SPN_SYSTEM.Ability.Str.abbr",
+  dex: "SPN_SYSTEM.Ability.Dex.abbr",
+  int: "SPN_SYSTEM.Ability.Int.abbr",
+  wis: "SPN_SYSTEM.Ability.Wis.abbr",
+};
